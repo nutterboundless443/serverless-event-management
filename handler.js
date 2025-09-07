fe765f18457b6e970a51615595801ec9d37d4c20
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-07 14:27:41 -0500

    Fix error handling message in createEvent function

--- handler.js
+++ handler.js
@@ -24,7 +24,7 @@ module.exports.createEvent = async (event) => {
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to create event!' })
+      body: JSON.stringify({ error: 'Failed to create the event!' })
     };
   }
 };
